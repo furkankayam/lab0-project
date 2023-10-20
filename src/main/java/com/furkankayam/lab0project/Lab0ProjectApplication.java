9190798b95782e0294563c8d7bffaa580edae5dc
Furkan Kaya <furkan.36kaya@gmail.com>
2023-10-20 22:18:49 +0300

    feat: initial commit

---
+++ src/main/java/com/furkankayam/lab0project/Lab0ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.furkankayam.lab0project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab0ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab0ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/furkankayam/lab0project/Lab0ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furkankayam.lab0project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab0ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
